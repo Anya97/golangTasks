7d9ed2974583333f7021a68988b53945c3f04ad6
Anya <Galickaya_ane4ka@bk.ru>
2023-11-11 23:09:18 +0300

    changed task

--- valid_brackets.go
+++ valid_brackets.go
@@ -3,22 +3,32 @@ package main
 import "fmt"
 
 func isBracketValid(s string) bool {
-	bracketsCount := 0
-	for _, v := range s {
-		if string(v) == "(" {
-			bracketsCount += 1
-		} else if string(v) == ")" {
-			bracketsCount -= 1
-		}
-		if bracketsCount < 0 {
-			return false
+	var stack []rune
+	openingBrackets := map[rune]bool{'(': true, '{': true, '[': true}
+	closingBrackets := map[rune]rune{')': '(', '}': '{', ']': '['}
+	for _, symbol := range s {
+		if isOpeningBrackets := openingBrackets[symbol]; isOpeningBrackets {
+			stack = append(stack, symbol)
+		} else if _, isClosingBrackets := closingBrackets[symbol]; isClosingBrackets {
+			if len(stack) == 0 {
+				return false
+			}
+			openingBracket := closingBrackets[symbol]
+			if stack[len(stack)-1] == openingBracket {
+				stack = stack[:len(stack)-1]
+			} else {
+				return false
+			}
 		}
 	}
-	return bracketsCount == 0
+	return len(stack) == 0
 }
 
 func main() {
 	fmt.Printf("%t\n", isBracketValid(")("))
+	fmt.Printf("%t\n", isBracketValid("(){}[]"))
+	fmt.Printf("%t\n", isBracketValid("([)]"))
+	fmt.Printf("%t\n", isBracketValid("({[()]})[]"))
 	fmt.Printf("%t\n", isBracketValid("("))
 	fmt.Printf("%t\n", isBracketValid("(())()"))
 	fmt.Printf("%t\n", isBracketValid("())("))
